462ee4cb4f44b0c62fcea4b9c60132886e2831f6
mstonya2023 <latonya.davis08@gmail.com>
2023-10-23 18:14:18 -0700

    created additional  models and api controllers

--- models/order.js
+++ models/order.js
@@ -2,19 +2,34 @@ const mongoose = require('mongoose');
 const itemSchema = require('./itemSchema');
 const Schema = mongoose.Schema;
 
-const PackageItemsSchema = new Schema ({
+const packageItemsSchema = new Schema ({
     qty: { type: Number, default: 1},
     item: itemSchema
 }, { 
-    timestamp: true 
-})
+    timestamp: true, 
+    toJSON: { virtuals: true}
+});
+
+packageItemsSchema.virtual('extPrice').get(function(){
+  // 'this' keyword is bound to the packageItem subdocument
+  return this.qty * this.item.price;
+});
 
 const orderSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    PackageItems: [PackageItemsSchema],
-    isPaid: { type: Boolean, default: false },
+    packageItems: [packageItemsSchema],
+    isPaid: { type: Boolean, default: false }
   }, {
     timestamps: true
   });
 
+  orderSchema.virtual('orderTotal').get(function(){
+  return this.packageItems.reduce((total, item) => total + item.ext.Price , 0);
+});
+  orderSchema.virtual('totalQty').get(function(){
+  return this.packageItems.reduce((total, item) => total + item.qty , 0);
+});
+  orderSchema.virtual('orderId').get(function(){
+  return this.id.slice(-6 .toUpperCase());
+});
   module.exports = mongoose.model('Order', orderSchema)
--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from "react";
+// import * as itemsAPI from '../../utilities/items-api';
+// import * as ordersAPI from '../../utilities/orders-api';
 export default function NewOrderPage() {
+  const [cart, setCart] = useState(null);
+// useEffect(function()  {
+// async function getCart() {
+//   const cart = await ordersAPI.getCart();
+//   setCart(cart);
+//   // categoriesRef.current = [...new Set(items.map)]
+
+// }
+// getItems();
+// }, []);
+  
+  
+  
   return (
+
+
     <h1>NewOrderPage</h1>
   );
 }
