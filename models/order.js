996588717fe43667d3423e6a12a88dc30d5e2f2d
mstonya2023 <latonya.davis08@gmail.com>
2023-10-23 17:19:02 -0700

    Created itemSchema and item models for packages

--- models/order.js
+++ models/order.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
+const itemSchema = require('./itemSchema');
 const Schema = mongoose.Schema;
 
 const PackageItemsSchema = new Schema ({
     qty: { type: Number, default: 1},
+    item: itemSchema
 }, { 
     timestamp: true 
 })
-
-
 
 const orderSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import './App.css';
-import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import NavBar from '../../components/NavBar/NavBar';
